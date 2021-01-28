67da8a269fff2a784ce7e28ccd95fb8dd9197930
Noah <76483681+Noah981107@users.noreply.github.com>
2021-01-28 13:37:06 +0900

    BaekJoon
    백준 알고리즘 문제풀이

---
+++ Bronze/src/bronze4/Solve_1330.java
@@ -0,0 +1,18 @@
+package bronze4;
+import java.util.Scanner;
+public class Solve_1330 {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>b)
+            System.out.println(">");
+        else if(a<b)
+            System.out.println("<");
+        else if(a==b)
+            System.out.println("==");
+        sc.close();
+	}
+
+}
---
+++ Bronze/src/module-info.java
@@ -0,0 +1,2 @@
+module bronze {
+}
