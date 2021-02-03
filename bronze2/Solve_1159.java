7aa5b1b589f692a9661025ed331796b6b5a706ee
Noah <76483681+Noah981107@users.noreply.github.com>
2021-02-03 13:26:31 +0900

    Renew Solve_1159
    Renew Solve_1159

--- bronze2/Solve_1159.java
+++ bronze2/Solve_1159.java
@@ -1,10 +1,24 @@
 package bronze2;
-
+ 
+import java.io.*;
+ 
 public class Solve_1159 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 
+        int n = Integer.parseInt(br.readLine());
+        int[] counts = new int[('z' - 'a') + 1];
+        for (int i = 0; i < n; i++)
+            counts[br.readLine().charAt(0) - 'a']++;
+ 
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < counts.length; i++)
+            if (counts[i] >= 5)
+                sb.append((char) ('a' + i));
+ 
+        bw.write(sb.toString().length() > 0 ? sb.toString() : "PREDAJA");
+        bw.close();
+    }
 }
