9690182016a47006f96225ff1dfde9e796aeb31a
Noah <76483681+Noah981107@users.noreply.github.com>
2021-02-05 13:56:50 +0900

    Solve_2420
    Solve_2420

---
+++ bronze4/Solve_2420.java
@@ -0,0 +1,13 @@
+package bronze4;
+import java.util.Scanner;
+import java.lang.Math;
+public class Solve_2420 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextInt();
+		long b = sc.nextInt();
+		System.out.println((int)Math.abs(a-b));
+	}
+
+}
