4477271f794db0e9641a904955c4cfb2309377b0
Noah <76483681+Noah981107@users.noreply.github.com>
2021-02-05 13:43:10 +0900

    Solve_1008
    Solve_1008

---
+++ bronze4/Solve_1008.java
@@ -0,0 +1,10 @@
+package bronze4;
+import java.util.Scanner;
+public class Solve_1008{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println((double)a / b);
+    }
+}
