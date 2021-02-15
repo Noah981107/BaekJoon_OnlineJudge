f860001be70082a6d2d1960c390a9d3496ceca0c
Noah <76483681+Noah981107@users.noreply.github.com>
2021-02-15 21:42:34 +0900

    Solve_1550
    Solve_1550

---
+++ bronze5/Solve_1550.java
@@ -0,0 +1,11 @@
+package bronze5;
+import java.util.*;
+ 
+class Main {
+  public static void main(String[] args) throws Exception {
+      Scanner sc=new Scanner(System.in);
+      String a=sc.next();
+      System.out.println(Integer.parseInt(a,16));
+      sc.close();
+   }
+}
