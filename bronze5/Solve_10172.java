5d3b67fb441d4cbd27535e87177f3aac371ec4cf
Noah <76483681+Noah981107@users.noreply.github.com>
2021-02-19 15:09:16 +0900

    Solve_10172
    Solve_10172

---
+++ bronze5/Solve_10172.java
@@ -0,0 +1,10 @@
+package bronze5;
+class Solve_10172{
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
