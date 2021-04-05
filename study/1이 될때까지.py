76dd242f760858f1993221a46618cc5954f4c08a
Noah <76483681+Noah981107@users.noreply.github.com>
2021-04-05 22:25:50 +0900

    1이 될때까지
    1이 될때까지

---
+++ study/1이 될때까지.py
@@ -0,0 +1,15 @@
+n, k = map(int,input().split())
+
+count = 0
+
+while True :
+    if n == 1 :
+        break
+    if n % k  != 0 :
+        count += 1
+        n -= 1
+    else :
+        count += 1
+        n //= k
+
+print(count)
