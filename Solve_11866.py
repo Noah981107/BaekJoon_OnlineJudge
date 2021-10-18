f0f567336b45da21e2cfb7b25c408166176a422c
Noah981107 <shgustmd6865@naver.com>
2021-10-18 21:03:07 +0900

    Solve_11866, 1018

--- Solve_11866.py
+++ Solve_11866.py
@@ -1,3 +1,21 @@
-import sys
+from collections import deque
+from sys import stdin
 
-n, k = map(int, sys.stdin.readline().split())
+queue = deque()
+result = []
+
+n, k = map(int, stdin.readline().split())
+
+for i in range(1, n+1):
+    queue.append(i)
+
+while queue:
+    for i in range(k-1):
+        queue.append(queue.popleft())
+    result.append(queue.popleft())
+
+print("<",end='')
+for i in range(len(result)-1):
+    print("%d, "%result[i], end='')
+print(result[-1], end='')
+print(">")
