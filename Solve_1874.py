bbee8572cbeced43b382ca8a05331ae6066ed2fe
Noah <shgustmd6865@naver.com>
2022-01-22 23:08:53 +0900

    Solve_1874

---
+++ Solve_1874.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+n = int(stdin.readline())
+s = []
+op = []
+count = 1
+temp = True
+for i in range(n):
+    num = int(stdin.readline())
+    while count <= num:
+        s.append(count)
+        op.append('+')
+        count += 1
+    if s[-1] == num:
+        s.pop()
+        op.append('-')
+    else:
+        temp = False
+if temp == False:
+    print('NO')
+else:
+    for i in op:
+        print(i)
