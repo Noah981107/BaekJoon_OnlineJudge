ff8e81d7493081a933ecb68be89089b0dcf87f8b
Noah <shgustmd6865@naver.com>
2022-07-18 13:33:55 +0900

    Solve_10989

---
+++ Solve_10989.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solve_10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[10001];
+        for(int i=0; i<N; i++){
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<10001; i++){
+            while(arr[i]>0){
+                sb.append(i).append('\n');
+                arr[i]--;
+            }
+        }
+        System.out.println(sb);
+    }
+}
