834286b6c50f6725780218b4c89d6f7a22c58297
Noah <shgustmd6865@naver.com>
2022-09-16 21:32:28 +0900

    Solve_2018

---
+++ Solve_2018.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solve_2018 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n  = Integer.parseInt(bf.readLine());
+        int count = 1;
+
+        for(int i = 1; i < (n / 2) + 1; i++) {
+            int sum = i;
+            for(int j = i + 1; i < (n / 2) + 2; j++) {
+                if(sum == n) {
+                    count++;
+                    break;
+                }
+                if(sum > n) {
+                    break;
+                }
+                sum += j;
+            }
+        }
+        System.out.println(count);
+    }
+}
