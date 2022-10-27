a37678e1c506b021dbb44c4f82ae622f02b40b9f
Noah <shgustmd6865@naver.com>
2022-10-27 13:27:45 +0900

    만들 수 없는 금액

---
+++ study/만들 수 없는 금액.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        int number = 1;
+        while (true) {
+
+            int tmp = number;
+            for (int i=0; i<n; i++) {
+                if (tmp > 0 && arr[i] <= tmp) {
+                    tmp -= arr[i];
+                }
+            }
+
+            if (tmp != 0)
+                break;
+
+            number++;
+        }
+
+        System.out.println(number);
+    }
+}
