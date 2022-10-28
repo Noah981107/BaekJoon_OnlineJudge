62d167de76ac3a6a400d9d5b190c09075c120b6a
Noah <shgustmd6865@naver.com>
2022-10-28 16:56:38 +0900

    Solve_1655

---
+++ Solve_1655.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Solve_1655 {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        while (N --> 0) {
+            int number = Integer.parseInt(br.readLine());
+
+            if (minHeap.size() == maxHeap.size())
+                maxHeap.offer(number);
+            else
+                minHeap.offer(number);
+
+            if (!minHeap.isEmpty() && !maxHeap.isEmpty()) {
+                if (minHeap.peek() < maxHeap.peek()) {
+                    int tmp = minHeap.poll();
+                    minHeap.offer(maxHeap.poll());
+                    maxHeap.offer(tmp);
+                }
+            }
+            sb.append(maxHeap.peek() + "\n");
+        }
+
+        System.out.print(sb);
+    }
+}
