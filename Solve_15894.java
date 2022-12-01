ebff5572fb10bfc13bd8216e62af9a49306b1fa6
Noah <shgustmd6865@naver.com>
2022-12-01 23:39:18 +0900

    Solve_15894

---
+++ Solve_15894.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solve_15894 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		System.out.print(Long.parseLong(br.readLine()) * 4);
+	}
+}
